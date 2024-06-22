8360e9118ac1314305bb07f67b962e89226a62f0
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-06-22 23:19:38 +0530

    added video ini sender component

--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -1,57 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export const Sender = () => {
-  const [socket, setSocket] = useState<WebSocket | null>();
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [pc, setPC] = useState<RTCPeerConnection | null>(null);
 
-  useEffect(() => {
-    const socket: WebSocket | null = new WebSocket("ws://localhost:8080");
-    setSocket(socket);
-    socket.onopen = () => {
-      socket.send(JSON.stringify({ type: "sender" }));
-    };
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+        setSocket(socket);
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'sender'
+            }));
+        }
+    }, []);
 
-    return ()=>{
-      socket.close();
+    const initiateConn = async () => {
+
+        if (!socket) {
+            alert("Socket not found");
+            return;
+        }
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'createAnswer') {
+                await pc.setRemoteDescription(message.sdp);
+            } else if (message.type === 'iceCandidate') {
+                pc.addIceCandidate(message.candidate);
+            }
+        }
+
+        const pc = new RTCPeerConnection();
+        setPC(pc);
+        pc.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket?.send(JSON.stringify({
+                    type: 'iceCandidate',
+                    candidate: event.candidate
+                }));
+            }
+        }
+
+        pc.onnegotiationneeded = async () => {
+            const offer = await pc.createOffer();
+            await pc.setLocalDescription(offer);
+            socket?.send(JSON.stringify({
+                type: 'createOffer',
+                sdp: pc.localDescription
+            }));
+        }
+            
+        getCameraStreamAndSend(pc);
     }
-  }, []);
 
-  async function startSendingVideo() {
-    if (!socket) return;
-    const pc = new RTCPeerConnection();
-
-    pc.onnegotiationneeded=async()=>{
-      console.log("negociation")
-      const offer = await pc.createOffer();
-      await pc.setLocalDescription(offer);
-      socket.send(
-        JSON.stringify({ type: "createOffer", sdp: pc.localDescription })
-      );
+    const getCameraStreamAndSend = (pc: RTCPeerConnection) => {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            const video = document.createElement('video');
+            video.srcObject = stream;
+            video.play();
+            // this is wrong, should propogate via a component
+            document.body.appendChild(video);
+            stream.getTracks().forEach((track) => {
+                pc?.addTrack(track);
+            });
+        });
     }
 
-    pc.onicecandidate = (event) => {
-      console.log(event);
-      socket.send(
-        JSON.stringify({ type: "iceCandidate", candidate: event.candidate })
-      );
-    };
-
-    socket.onmessage = async (event) => {
-      const message = JSON.parse(event.data);
-      if (message.type === "createAnswer") {
-        await pc.setRemoteDescription(message.sdp);
-      } else if (message.type === "iceCandidate") {
-        await pc.addIceCandidate(message.candidate);
-      }
-    };
-
-    const stream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
-    pc.addTrack(stream.getTracks()[0])
-  }
-
-  return (
-    <div>
-      Sender
-      <button onClick={startSendingVideo}>Send Video</button>
+    return <div>
+        Sender
+        <button onClick={initiateConn}> Send data </button>
     </div>
-  );
-};
+}
